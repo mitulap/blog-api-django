dd42c81f3911984d77b543786f4bfa3806c93053
mitul <mitulpatel.hsd@gmail.com>
2016-07-03 19:34:06 -0700

    adding source

---
+++ src/comments/api/serializers.py
@@ -0,0 +1,128 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+
+from rest_framework.serializers import (
+	HyperlinkedIdentityField,
+	ModelSerializer,
+	SerializerMethodField,
+	ValidationError
+	)
+
+from comments.models import Comment
+
+User = get_user_model()
+
+def create_comment_serializer(model_type='post', slug=None, parent_id=None, user=None):
+	class CommentCreateSerializer(ModelSerializer):
+		class Meta:
+			model = Comment
+			fields = [
+				'id',
+				'content',
+				'timestamp'
+			]
+
+		def __init__(self, *args, **kwargs):
+			self.model_type = model_type
+			self.slug = slug
+			self.parent_obj = None
+			if parent_id:
+				parent_qs = Comment.objects.filter(id=parent_id)
+				if parent_qs.exists() and parent_qs.count() == 1:
+					self.parent_obj = parent_qs.first()
+			return super(CommentCreateSerializer, self).__init__(*args, **kwargs)
+
+		def validate(self, data):
+			model_type = self.model_type
+			model_qs = ContentType.objects.filter(model=model_type)
+			if not model_qs.exists() or model_qs.count() != 1:
+				raise ValidationError("This is not a valid content type")
+			SomeModel = model_qs.first().model_class()
+			obj_qs = SomeModel.objects.filter(slug=self.slug)
+			if not obj_qs.exists() or obj_qs.count() != 1:
+				raise ValidationError("This is not a slug for this content type")
+			return data
+
+		def create(self, validated_data):
+			content = validated_data.get("content")
+			if user:
+				main_user = user
+			else:
+				main_user = User.objects.all().first()
+			model_type = self.model_type
+			slug = self.slug
+			parent_obj = self.parent_obj
+			comment = Comment.objects.create_by_model_type(
+					model_type,slug,content,main_user,
+					parent_obj=parent_obj,
+				)
+			return comment
+
+
+	return CommentCreateSerializer
+
+class CommentSerializer(ModelSerializer):
+	reply_count = SerializerMethodField()
+	class Meta:
+		model = Comment
+		fields = [
+			'id',
+			'content_type',
+			'object_id',
+			'parent',
+			'content',
+			'reply_count',
+			'timestamp',
+		]
+
+	def get_reply_count(self, obj):
+		if obj.is_parent:
+			return obj.children().count()
+
+		return 0
+
+
+
+class CommentChildSerializer(ModelSerializer):
+	class Meta:
+		model = Comment
+		fields = [
+			'id',
+			'content',
+			'timestamp',
+		]
+
+class CommentDetailSerializer(ModelSerializer):
+	reply_count = SerializerMethodField()
+	replies = SerializerMethodField()
+	class Meta:
+		model = Comment
+		fields = [
+			'id',
+			'content_type',
+			'object_id',
+			'content',
+			'reply_count',
+			'replies',
+			'timestamp',
+		]
+
+	def get_replies(self, obj):
+		if obj.is_parent:
+			return CommentChildSerializer(obj.children(), many=True).data
+		return None
+	def get_reply_count(self, obj):
+		if obj.is_parent:
+			return obj.children().count()
+		return 0
+
+
+class CommentEditSerializer(ModelSerializer):
+	
+	class Meta:
+		model = Comment
+		fields = [
+			'id',
+			'content',
+			'timestamp',
+		]
---
+++ src/posts/api/serializers.py
@@ -0,0 +1,111 @@
+from rest_framework.serializers import (
+	HyperlinkedIdentityField,
+	ModelSerializer,
+	SerializerMethodField
+	)
+
+from comments.api.serializers import CommentSerializer
+from comments.models import Comment
+
+from posts.models import Post
+
+class PostCreateUpdateSerializer(ModelSerializer):
+	class Meta:
+		model = Post
+		fields = [
+			# 'id',
+			'title',
+			# 'slug',
+			'content',
+			'publish'
+		]
+
+
+post_detail_url = HyperlinkedIdentityField(
+		view_name='posts-api:detail',
+		lookup_field='slug'
+		)
+
+class PostDetailSerializer(ModelSerializer):
+	url = post_detail_url
+	user = SerializerMethodField()
+	image = SerializerMethodField()
+	html = SerializerMethodField()
+	comments = SerializerMethodField()
+	class Meta:
+		model = Post
+		fields = [
+			'url',
+			'user',
+			'id',
+			'title',
+			'slug',
+			'content',
+			'html',
+			'publish',
+			'image',
+			'comments'
+		]
+
+	def get_html(self, obj):
+		return obj.get_markdown()
+
+	def get_user(self, obj):
+		return str(obj.user.username)
+
+	def get_image(self, obj):
+		try:
+			image = obj.image.path
+		except:
+			image = None
+
+		return image
+
+	def get_comments(self, obj):
+		c_qs = 	Comment.objects.filter_by_instance(obj)
+		comments = CommentSerializer(c_qs, many=True).data
+		return comments
+
+class PostListSerializer(ModelSerializer):
+	url = post_detail_url
+	user = SerializerMethodField()
+
+	# delete_url = HyperlinkedIdentityField(
+	# 	view_name='posts-api:delete',
+	# 	lookup_field='slug'
+	# 	)
+
+	class Meta:
+		model = Post
+		fields = [
+			'url',
+			'user',
+			'title',
+			'content',
+			'publish'
+		]
+
+	def get_user(self, obj):
+		return str(obj.user.username)
+
+
+"""
+
+from posts.models import Post
+from posts.api.serializers import PostDetailSerializer
+
+data = {
+        "title": "Yeah Buddy!",
+        "content": "New Content",
+        "publish": "2016-02-12",
+        "slug" : "yeah-buddy",
+    }
+
+
+obj = Post.objects.get(id=3)
+new_item = PostDetailSerializer(obj, data=data)
+if new_item.is_valid():
+	new_item.save()
+else:
+	print(new_item.errors)
+"""
