4a1d22033eb04ddb806575219c108c9bfb19121f
mitul <mitulpatel.hsd@gmail.com>
2016-07-05 00:27:18 -0700

    Adding user API with serializer

---
+++ src/accounts/api/serializers.py
@@ -0,0 +1,38 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+
+from rest_framework.serializers import (
+	HyperlinkedIdentityField,
+	ModelSerializer,
+	SerializerMethodField,
+	ValidationError
+	)
+
+User = get_user_model()
+
+class UserCreateSerializer(ModelSerializer):
+	class Meta:
+		model = User
+		fields = [
+			'username',
+			'email',
+			'password',
+
+		]
+
+		extra_kwargs = {"password":
+							{"write_only": True}
+						}
+
+	def create(self, validated_data):
+		username = validated_data['username']
+		email = validated_data['email']
+		password = validated_data['password']
+		user_obj = User(
+				username = username,
+				email = email,
+
+			)
+		user_obj.set_password(password)
+		user_obj.save()
+		return validated_data
